0cb55ed2550a8de2a024d13b34a046f882dd386a
nikhila <nkmp@simcorp.com>
2022-08-29 09:16:08 +0200

    feature 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,8 @@
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World Nikhila!");
+            Console.WriteLine("Hello World ");
+            Console.WriteLine("feature barnch 2 - development 2 ");
         }
     }
 }
