f0ffd257d43d97ee505189dc1819f8adbfb68168
nikhila <nkmp@simcorp.com>
2022-08-29 08:47:28 +0200

    Added message for console oputput

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,7 @@
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Nikhila!");
         }
     }
 }
