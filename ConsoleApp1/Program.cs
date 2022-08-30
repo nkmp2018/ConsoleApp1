46b0a8b1312f7ca7942560cc26d55a36a4398d0c
nikhila <nkmp@simcorp.com>
2022-08-30 12:07:40 +0200

    cherry picked from master

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("Hello World Nikhila!");
+            Console.WriteLine("Feature Branch 1 - Development 2");
             Console.WriteLine("feature branch 1 - development 1!");
         }
     }
