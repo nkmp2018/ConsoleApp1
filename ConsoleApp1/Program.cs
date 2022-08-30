3a3b8fa7cae0275516e225b1f01fe9933814ef5d
nikhila <nkmp@simcorp.com>
2022-08-30 12:12:01 +0200

    Revert "new line to master"
    This reverts commit b917919f55abb0173e7023854bbe358ec84be0fb.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,6 @@ namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World Nikhila!");
-            Console.WriteLine("Feature Branch 1 - Development 2");
         }
     }
 }
