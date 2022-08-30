b917919f55abb0173e7023854bbe358ec84be0fb
nikhila <nkmp@simcorp.com>
2022-08-30 12:07:40 +0200

    new line to master

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World Nikhila!");
+            Console.WriteLine("Feature Branch 1 - Development 2");
         }
     }
 }
